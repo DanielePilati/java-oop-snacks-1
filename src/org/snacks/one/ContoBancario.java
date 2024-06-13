8abf6342926bf5fa3f3c2ee0f0abf86473748bda
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-06-13 20:43:44 +0200

    end

--- src/org/snacks/one/ContoBancario.java
+++ src/org/snacks/one/ContoBancario.java
@@ -1,5 +1,7 @@
 package org.snacks.one;
 
+import java.util.Scanner;
+
 public class ContoBancario {
 
 	private int numeroConto;
@@ -9,19 +11,15 @@ public class ContoBancario {
 		this.saldo = 0;
 		this.numeroConto = numeroConto;
 	}
-	
 	public int getNumeroConto() {
 		return numeroConto;
 	}
-
 	public void setNumeroConto(int numeroConto) {
 		this.numeroConto = numeroConto;
 	}
-
 	public double getSaldo() {
 		return saldo;
 	}
-
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
@@ -37,13 +35,18 @@ public class ContoBancario {
 	
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+		ContoBancario mioConto = new ContoBancario(120356);
+		
+		System.out.println("saldo attuale: " + mioConto.getSaldo());
+		System.out.println("inserisci importo da prelevare");
+		mioConto.preleva(input.nextDouble());
+		System.out.println("saldo attuale: " + mioConto.getSaldo());
+		System.out.println("inserisci importo da versare");
+		mioConto.versa(input.nextDouble());
+		System.out.println("saldo attuale: " + mioConto.getSaldo());
 
 	}
-
-
-
-
-
 
 
 
