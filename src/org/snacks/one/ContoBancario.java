583d47d3c024a0b0415404f49fa090bac23702dc
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-06-13 20:38:30 +0200

    add preleva

--- src/org/snacks/one/ContoBancario.java
+++ src/org/snacks/one/ContoBancario.java
@@ -10,7 +10,25 @@ public class ContoBancario {
 		this.numeroConto = numeroConto;
 	}
 	
-	
+	public int getNumeroConto() {
+		return numeroConto;
+	}
+
+	public void setNumeroConto(int numeroConto) {
+		this.numeroConto = numeroConto;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public double preleva(double importo) {
+		 this.saldo -= importo;
+		 return saldo;
+	}
 	
 	
 	public static void main(String[] args) {
@@ -23,31 +41,6 @@ public class ContoBancario {
 
 
 
-	public int getNumeroConto() {
-		return numeroConto;
-	}
 
-
-
-
-
-
-	public void setNumeroConto(int numeroConto) {
-		this.numeroConto = numeroConto;
-	}
-
-
-
-
-	public double getSaldo() {
-		return saldo;
-	}
-
-
-
-
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
 
 }
