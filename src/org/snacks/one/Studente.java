3bd249384e716e080d82ee1921958a668156ef5c
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-06-13 20:20:41 +0200

    add snack

--- src/org/snacks/one/Studente.java
+++ src/org/snacks/one/Studente.java
@@ -1,10 +1,26 @@
 package org.snacks.one;
 
 public class Studente {
+	
+	
+		private String nome;
+		private String cognome;
+		private int eta;
 
+		Studente(String nome,String cognome,int eta){
+			this.nome = nome;
+			this.cognome = cognome;
+			this.eta = eta;
+		}
+		public String description () {
+			return this.nome + " " + this.cognome + ", " + this.eta;
+		}
+		
+		
+		
+		
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
