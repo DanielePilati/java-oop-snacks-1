28cc687c1546cb77909957ff6367a616a980a3cd
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-06-29 09:27:55 +0200

    add class

---
+++ src/org/snacks/four/Main.java
@@ -0,0 +1,24 @@
+package org.snacks.four;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<String> arrayList = new ArrayList<String>();
+		
+		arrayList.add("Geena");
+		arrayList.add("Geeno");
+		arrayList.add("Geene");
+		arrayList.add("Geeni");
+		arrayList.add("Geenu");
+		arrayList.add("Geenatti");
+		arrayList.add("Geenotti");
+		arrayList.add("Geenetti");
+		arrayList.add("Geenitti");
+		arrayList.add("Geenutti");
+		
+		
+	}
+
+}
